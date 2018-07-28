82348ea69b5e2f16c03761338af1b126957db8d2
MYOB\Claudiu.Socaci <claudiu.socaci@myob.com>
2018-07-28 14:07:20 +1200

    Added working scrapping service

--- KostoAPIService.js
+++ KostoAPIService.js
@@ -1,12 +1,37 @@
-const express = require('express');
-const osmosis = require('osmosis');
+const express = require('express')();
+const xray = require('x-ray')();
 
 const port = process.env.PORT || 3000;
 
-let app = express();
+express.get('/getAverageCostOfMilk/:city',(req,res)=>{
+    
+    var city = req.params.city;
+    
+    console.log(city);
+    
+    xray(`https://www.expatistan.com/cost-of-living/${city}`,
+    '.single-city tbody tr:nth-child(5) td.price').then( function(res1){
+            
+        xray(`https://www.numbeo.com/cost-of-living/in/${city}`,
+        '.data_wide_table tr:nth-child(11) td:nth-child(2)').then(function(res2){
 
-app.get('/',(req,res)=>{
-    res.send({success:'First call succeeded.'});
+            let regex = /[+-]?\d+(\.\d+)?/g;
+
+            res1 = res1.match(regex).map(function(v) { return parseFloat(v); })[0];
+            res2 = res2.match(regex).map(function(v) { return parseFloat(v); })[0];
+
+            console.log(res1,res2);
+
+            res.send((res1+res2)/2.0+'');
+            
+        });
+    });
 });
 
-app.listen(port, ()=>{console.log(`Server is up on port ${port};`);});
+// express.get('/getCostFromNumbeo/:city',(req,res)=>{
+//     var city = req.params.city;
+//     console.log(city);
+//     stream.pipe(res);
+// });
+
+express.listen(port, ()=>{console.log(`Server is up on port ${port};`);});
