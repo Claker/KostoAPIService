5b64813146ebe28709d901bb7c2b4b5e6bf4d960
MYOB\Claudiu.Socaci <claudiu.socaci@myob.com>
2018-07-28 14:17:58 +1200

    clean up

--- KostoAPIService.js
+++ KostoAPIService.js
@@ -1,13 +1,12 @@
 const express = require('express')();
 const xray = require('x-ray')();
 
+const intAndFloatsRegex = /[+-]?\d+(\.\d+)?/g;
 const port = process.env.PORT || 3000;
 
 express.get('/getAverageCostOfMilk/:city',(req,res)=>{
     
     var city = req.params.city;
-    
-    console.log(city);
     
     xray(`https://www.expatistan.com/cost-of-living/${city}`,
     '.single-city tbody tr:nth-child(5) td.price').then( function(res1){
@@ -15,23 +14,12 @@ express.get('/getAverageCostOfMilk/:city',(req,res)=>{
         xray(`https://www.numbeo.com/cost-of-living/in/${city}`,
         '.data_wide_table tr:nth-child(11) td:nth-child(2)').then(function(res2){
 
-            let regex = /[+-]?\d+(\.\d+)?/g;
-
-            res1 = res1.match(regex).map(function(v) { return parseFloat(v); })[0];
-            res2 = res2.match(regex).map(function(v) { return parseFloat(v); })[0];
-
-            console.log(res1,res2);
+            res1 = parseFloat(res1.match(intAndFloatsRegex));
+            res2 = parseFloat(res2.match(intAndFloatsRegex));
 
             res.send((res1+res2)/2.0+'');
-            
         });
     });
 });
-
-// express.get('/getCostFromNumbeo/:city',(req,res)=>{
-//     var city = req.params.city;
-//     console.log(city);
-//     stream.pipe(res);
-// });
 
 express.listen(port, ()=>{console.log(`Server is up on port ${port};`);});
